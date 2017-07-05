700354f60cbd0b67f45a477f3d9f2432c4ba5d7d
Federico Martinez <famfededeveloper@gmail.com>
2017-07-04 22:29:39 -0300

    Quita conflictos

--- analyzer/parser.py
+++ analyzer/parser.py
@@ -11,18 +11,18 @@ import objetoParseado as op
 #     else:
 #         p[0] = (str(p[1]+' '+p[2][0]+' '+p[3]+' '+p[4][0]+' '+p[5]+' '+p[6][0]), '('+p[2][1]+'->'+p[6][1]+')')
 
-def p_b_iszero(p):
+def p_exp_iszero(p):
     'expression : ISZERO LPAREN expression RPAREN'
     if p[3].getTipo() != 'Nat':
         return "Error: isZero espera un Nat"
     else:
         p[0] = op.objetoParseado('isZero('+p[3].getExpresion()+')', 'Bool', str(p[3].getValor() == 0))
 
-def p_expression_true(p):
+def p_exp_true(p):
     'expression : TRUE'
     p[0] = op.objetoParseado("True", 'Bool', 'True')
 
-def p_expression_false(p):
+def p_exp_false(p):
     'expression : FALSE'
     p[0] = op.objetoParseado("False", 'Bool', 'False')
 
@@ -33,17 +33,13 @@ def p_expression_succ(p):
     
     p[0] = op.objetoParseado('succ('+str(p[3].getExpresion())+')', 'Nat', 'succ('+str(p[3].getValor())+')')
 
-def p_exp_pred(p):
-    'expression : PRED LPAREN expression RPAREN'
-    valor = 'pred('+str(p[3].getValor())+')'
-    if p[3].getValor() == 0:
-        valor = 0
-
-    p[0] = op.objetoParseado('pred('+str(p[3].getExpresion())+')', 'Nat', valor)
-
 def p_exp_pred_succ(p):
     'expression : PRED LPAREN SUCC LPAREN expression RPAREN RPAREN'
     p[0] = op.objetoParseado(str(p[5].getExpresion()), 'Nat', p[5].getValor())
+
+def p_exp_pred(p):
+    'expression : PRED LPAREN ZERO RPAREN'
+    p[0] = op.objetoParseado('pred('+str(p[3])+')', 'Nat', 0)
 
 def p_exp_zero(p):
     'expression : ZERO'
